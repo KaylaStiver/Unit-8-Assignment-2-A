8248c723b43996baaad7dbce370180c0ded2c8d3
KaylaStiver <KaylaStiver3@gmail.com>
2022-04-28 08:25:51 -0400

    Finished project. Added object with exception classes and converted existing functions to member functions.

--- Unit 8 Assignment A/Source.cpp
+++ Unit 8 Assignment A/Source.cpp
@@ -7,32 +7,86 @@
 
 using namespace std;
 
-char character(char, int);
+class Exceptions {
+private:
+	char start;
+	int offset;
+	char returnChar;
+public:
+	Exceptions() {
+		start = 'A';
+		offset = 0;
+		returnChar = 'B';
+	}
+
+	class invalidCharacterException {};
+	class invalidRangeException {};
+
+	void setStart(char);
+	void setOffset(int);
+	char getStart() const;
+	int getOffset() const;
+	char character(char, int);
+};
 
 int main() {
 
-	char start = 'A';
-	int offset = 0;
-	char returnChar = 'A';
+	char start;
+	int offset;
+	char returnChar;
+
+	Exceptions myException;
 
 	cout << "Please enter start character: ";
 	cin >> start;
 	cout << "Please enter offset: ";
 	cin >> offset;
-	returnChar = character(start, offset);
-	cout << "Returned character is: " << returnChar << endl;
 
+	try {
+		myException.setStart(start);
+		myException.setOffset(offset);
+		returnChar = myException.character(start, offset);
+		cout << "Returned character is: " << returnChar << endl;
+	}
+	catch (Exceptions::invalidCharacterException) {
+		cout << "Error: invalidCharacterException thrown." << endl;
+	}
+	catch (Exceptions::invalidRangeException) {
+		cout << "Error: invalidRangeException thrown." << endl;
+	}
+	
 	return 0;
 }
-char character(char start, int offset) {
+void Exceptions::setStart(char expChar) {
+	if (isalpha(expChar) == 0) {
+		throw invalidCharacterException();
+	}
+	start = expChar;
+}
+void Exceptions::setOffset(int expInt) {
+	if (start == 26 && expInt > 0) {
+		throw invalidRangeException();
+	}
+	offset = expInt;
+}
+char Exceptions::getStart() const {
+	return start;
+}
+int Exceptions::getOffset() const {
+	return offset;
+}
+char Exceptions::character(char start, int offset) {
 	char alphaUpper[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char alphaLower[27] = "abcdefghijklmnopqrstuvwxyz";
 	for (int i = 0; i < 27; i++) {
 		if (isupper(start) != 0) {
 			if (start == alphaUpper[i]) {
 				i += offset;
+				if (i < 0) {
+					throw invalidRangeException();
+				}
 				if (i > 26) {
-					i -= 26;
+					throw invalidRangeException();
 				}
 				return alphaUpper[i];
 			}
@@ -40,8 +94,11 @@ char character(char start, int offset) {
 		if (islower(start) != 0) {
 			if (start == alphaLower[i]) {
 				i += offset;
+				if (i < 0) {
+					throw invalidRangeException();
+				}
 				if (i > 26) {
-					i -= 26;
+					throw invalidRangeException();
 				}
 				return alphaLower[i];
 			}
